640915d9a0199d9d01d879753bcad821fe323e97
Stephen Peters <7speter@gmail.com>
2020-02-22 20:05:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(line, newPerson){
+  line.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${line.length} in line.`
+}
+
+function nowServing(arr){
+
+}
+
+function currentLine(){
+
+}
